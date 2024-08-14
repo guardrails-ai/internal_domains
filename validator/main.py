c3d55242bec8509eb01c044149450f61dd4d4100
Aarav Navani <arav.navani@gmail.com>
2024-08-14 10:35:11 -0700

    readme

--- validator/main.py
+++ validator/main.py
@@ -1,11 +1,3 @@
-
-"""
-This template is intended for creating simple validators.
-
-If your validator is complex or requires additional post-installation steps, consider using the template repository instead.
-
-The template repository can be found here: https://github.com/guardrails-ai/validator-template
-"""
 import re
 
 from typing import Any, Callable, Dict, Optional, List
@@ -107,26 +99,4 @@ class InternalDomains(Validator):
         else:
             return PassResult()
 
-# Run tests via `pytest -rP ./internal_domains.py`
-class TestInternalDomains:
-    def test_success_case(self):
-        pass_input = """
-        This is a string with no domains except a reference to https://www.example.com  
-        """
-        validator = InternalDomains(internal_domains=["internal.company.com"])
-        result = validator.validate(pass_input, {})
-        assert isinstance(result, PassResult) is True
 
-    def test_failure_case(self):
-        fail_input = """
-This is a comprehensive resource for articles and information related to our company policies and procedures. 
-You can access it [here](https://kb.internal.company.com/api/v1/articles). You can learn more about projects [here](https://project-x.company.com/api/v1/articles).
-"""
-        validator = InternalDomains(internal_domains=["internal.company.com", "project-x.company.com"])
-        result = validator.validate(fail_input, {})
-        assert isinstance(result, FailResult) is True
-        assert result.error_message == "Found internal domains: https://kb.internal.company.com/api/v1/articles, https://project-x.company.com/api/v1/articles"
-        assert result.fix_value == """
-This is a comprehensive resource for articles and information related to our company policies and procedures. 
-You can access it [here](***********************************************). You can learn more about projects [here](*********************************************).
-"""
